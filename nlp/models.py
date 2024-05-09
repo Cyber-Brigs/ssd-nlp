79ab7cdd0f04477a0cf42d53dec9b9c7e532598c
steve <austines811@gmail.com>
2024-05-09 19:50:59 +0300

    creates a class to manange instances of a topic modelling procedure from LDA script and the necessary attriute.

--- nlp/models.py
+++ nlp/models.py
@@ -1,4 +1,5 @@
 import uuid
+from django.conf import settings
 from django.db import models
 from django.conf import settings
 from users.models import UserUpload
@@ -16,3 +17,13 @@ class TextProcessing(models.Model):
 
     def __str__(self):
         return f'TextProcessing ID: {self.id}'
+        
+class LdaTopicModelling(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user_upload = models.ForeignKey(UserUpload, on_delete=models.CASCADE)
+    text_processing = models.ForeignKey(TextProcessing, on_delete=models.CASCADE)
+    lda_topics_file_path = models.FileField(upload_to='topic_models/lda/')
+    selected_topics = models.PositiveIntegerField(blank=True, null=True)
+    coherence_value = models.FloatField(blank=True, null=True, max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
