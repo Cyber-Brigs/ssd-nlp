80597e35704cebbf87ac7142d3df6853fa164f3e
steve <austines811@gmail.com>
2024-05-09 19:53:02 +0300

    adds a serializer and url for the LDA script interface

--- nlp/serializers.py
+++ nlp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import TextProcessing
+from .models import TextProcessing, LdaTopicModelling
 
 class TextProcessingSerializer(serializers.ModelSerializer):
     user_id = serializers.ReadOnlyField(source='user.id')
@@ -23,3 +23,17 @@ class TextProcessingSerializer(serializers.ModelSerializer):
     class Meta:
         model = TextProcessing
         fields = ('id', 'user_id', 'user_upload_id','start_page', 'end_page', 'processed_text_file', 'corpus_file', 'dictionary_file', 'time_created')
+
+class LdaTopicModellingSerializer(serializers.ModelSerializer):
+    user_id = serializers.ReadOnlyField(source='user.id')
+    user_upload_id = serializers.ReadOnlyField(source='user_upload.id')
+    text_processing_id = serializers.ReadOnlyField(source='text_processing.id')
+    lda_topics_file_path = serializers.SerializerMethodField()
+    time_created = serializers.DateTimeField(source='created_at')
+
+    def get_lda_topics_file_path(self, instance):
+        return instance.lda_topics_file_path.name if instance.lda_topics_file_path else None
+
+    class Meta:
+        model = LdaTopicModelling
+        fields = ('id', 'user_id', 'user_upload_id', 'text_processing_id', 'lda_topics_file_path', 'selected_topics', 'coherence_value', 'time_created')
--- nlp/urls.py
+++ nlp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TextProcess, ProcessedDocsListView
+from .views import TextProcess, ProcessedDocsListView, LdaTopics, LdaTopicsSave, LdaTopicModellingListView
 
 urlpatterns = [
     path('process-text/', TextProcess.as_view(), name='process-text'),
-    path('processed-texts/', ProcessedDocsListView.as_view(), name='processed-texts')
+    path('processed-texts/', ProcessedDocsListView.as_view(), name='processed-texts'),
+    path('lda-model-topics/', LdaTopics.as_view(), name='lda-topics'),
+    path('lda-select-topics/<uuid:pk>/', LdaTopicsSave.as_view(), name='lda-selected-topics'),
+    path('lda-models-view/', LdaTopicModellingListView.as_view(), name='lda-view-models')
 ]
