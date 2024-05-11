3fda65696643c7dec48a0907003fdf66b9b2802d
steve <austines811@gmail.com>
2024-05-11 12:29:07 +0300

    adds a serializer and urls for lsa model actions

--- nlp/serializers.py
+++ nlp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import TextProcessing, LdaTopicModelling
+from .models import TextProcessing, LdaTopicModelling, LsaTopicModelling
 
 class TextProcessingSerializer(serializers.ModelSerializer):
     user_id = serializers.ReadOnlyField(source='user.id')
@@ -37,3 +37,17 @@ class LdaTopicModellingSerializer(serializers.ModelSerializer):
     class Meta:
         model = LdaTopicModelling
         fields = ('id', 'user_id', 'user_upload_id', 'text_processing_id', 'lda_topics_file_path', 'selected_topics', 'coherence_value', 'time_created')
+
+class LsaTopicModellingSerializer(serializers.ModelSerializer):
+    user_id = serializers.ReadOnlyField(source='user.id')
+    user_upload_id = serializers.ReadOnlyField(source='user_upload.id')
+    text_processing_id = serializers.ReadOnlyField(source='text_processing.id')
+    lsa_topics_file_path = serializers.SerializerMethodField()
+    time_created = serializers.DateTimeField(source='created_at')
+
+    def get_lsa_topics_file_path(self, instance):
+        return instance.lsa_topics_file_path.name if instance.lsa_topics_file_path else None
+
+    class Meta:
+        model = LsaTopicModelling
+        fields = ('id', 'user_id', 'user_upload_id', 'text_processing_id', 'lsa_topics_file_path', 'selected_topics', 'coherence_value', 'time_created')
--- nlp/urls.py
+++ nlp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import TextProcess, ProcessedDocsListView, LdaTopics, LdaTopicsSave, LdaTopicModellingListView
+from .views import TextProcess, ProcessedDocsListView, LdaTopics, LdaTopicsSave, LdaTopicModellingListView, LsaTopics, LsaTopicsSave, LsaTopicModellingListView
 
 urlpatterns = [
     path('process-text/', TextProcess.as_view(), name='process-text'),
     path('processed-texts/', ProcessedDocsListView.as_view(), name='processed-texts'),
     path('lda-model-topics/', LdaTopics.as_view(), name='lda-topics'),
     path('lda-select-topics/<uuid:pk>/', LdaTopicsSave.as_view(), name='lda-selected-topics'),
-    path('lda-models-view/', LdaTopicModellingListView.as_view(), name='lda-view-models')
+    path('lda-models-view/', LdaTopicModellingListView.as_view(), name='lda-view-models'),
+    path('lsa-model-topics/', LsaTopics.as_view(), name='lsa-topics'),
+    path('lsa-select-topics/<uuid:pk>/', LsaTopicsSave.as_view(), name='lsa-selected-topics'),
+    path('lsa-models-view/', LsaTopicModellingListView.as_view(), name='lsa-view-models')
 ]
